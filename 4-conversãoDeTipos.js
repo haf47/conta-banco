f5492b42276e871b1d0ff2dd47a6e7a39ea43207
Hércules  Ferreira <123484918+haf47@users.noreply.github.com>
2024-02-03 14:56:24 -0300

    Add files via upload

---
+++ 4-conversãoDeTipos.js
@@ -0,0 +1,10 @@
+console.log ("Conversão de tipos");
+
+console.log ("ano" + 2020); 
+console.log ("2" + "2");
+
+console.log (parseInt ("2") + parseInt ("2") ); // Nesse caso da soma, houve uma concatenação. Textos em números.
+
+console.log ("10" / "2"); //Especialmente em divisões o retorno será 5. porque divisão só faz sentido com número.
+
+console.log ("Ricardo" / "2"); // retornará NAN = Not a Number
