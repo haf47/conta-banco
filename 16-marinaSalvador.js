127618a0fe3c35643d03e4a42c1001f107ddb735
Hércules  Ferreira <123484918+haf47@users.noreply.github.com>
2024-02-03 15:03:33 -0300

    Add files via upload

---
+++ 16-marinaSalvador.js
@@ -0,0 +1,16 @@
+console.log ("Controle entrada e saída de embarcações no terminal contorno.");
+
+const permissaoAtracagem = 5;
+console.log (permissaoAtracagem);
+
+console .log ("Devido a profundidade,liberação só para embarcações leves.")
+
+if (permissaoAtracagem <= 4) {
+
+  console.log  ("Transito liberado, boa viajem.");
+    
+} else {
+
+    console.log ("Por favor,aguarde.");
+    
+}
