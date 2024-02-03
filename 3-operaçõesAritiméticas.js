88ffeb47c25dbb289515829a1d742fe86b101620
Hércules  Ferreira <123484918+haf47@users.noreply.github.com>
2024-02-03 14:55:41 -0300

    Add files via upload

---
+++ 3-operaçõesAritiméticas.js
@@ -0,0 +1,10 @@
+console.log ("Operaões aritiméticas");
+
+console.log (10+2);
+console.log (10+8*2);
+console.log ((10+8)*2);
+
+console.log ("ano" + 2020); // Um texto e um número? // JS vai juntar ambos = ano2020.
+console.log ("2" + "2");// Nesse caso ficaará assim 22.
+
+
