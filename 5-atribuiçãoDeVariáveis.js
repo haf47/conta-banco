19902adb01df7bb6c1a67938f1dac4123d96d847
Hércules  Ferreira <123484918+haf47@users.noreply.github.com>
2024-02-03 14:57:57 -0300

    Add files via upload

---
+++ 5-atribuiçãoDeVariáveis.js
@@ -0,0 +1,19 @@
+console.log ("Trabalhando com atribuição de variávis");
+
+const idade = 62;
+
+const nome = "Hercules";
+
+const sobrenome = "Frreira";
+
+console.log (nome);
+
+console.log (sobrenome);
+
+console.log (nome + sobrenome);//Fazendo assim, sairá HerculesFerreira
+console.log (nome + " " + sobrenome);// retornará Hercules Ferreira.
+
+console.log (nome,sobrenome);// Dessa forma sairá Hercules Ferreira
+
+console.log (`Meu nome é ${nome}`); // retornará Meu nome é Hercules.
+
