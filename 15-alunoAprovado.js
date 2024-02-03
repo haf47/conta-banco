be53a1baffbabe7946fc8ebf9071dc8c05649a75
Hércules  Ferreira <123484918+haf47@users.noreply.github.com>
2024-02-03 15:03:00 -0300

    Add files via upload

---
+++ 15-alunoAprovado.js
@@ -0,0 +1,32 @@
+console.log ("Aluno aprovado.Canadá embarque imediato.");
+
+const nomeAlunoAprovado = ("Pedro Borges");
+                        
+console.log (nomeAlunoAprovado);
+
+console.log ("classificâo 10");
+
+if (nomeAlunoAprovado === "Pedro Borges") {
+
+    console.log ("Welcome");
+    
+} else {
+    console.log ("Fica para o ano que vem, sorry");
+    
+}
+
+console.log ("Você quer tente acertar o número secreto?");
+
+const unicoNumeroRespostaCerta = 2;
+
+console.log (unicoNumeroRespostaCerta);
+
+if (unicoNumeroRespostaCerta === 28) {
+
+    console.log ("você acertou");
+    
+} else {
+    console.log ("Êita ferro, você errou!");
+    
+}
+
