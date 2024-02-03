1c215be5ddfd17538af53783f96f8015796ca30d
Hércules  Ferreira <123484918+haf47@users.noreply.github.com>
2024-02-03 14:49:40 -0300

    Add files via upload

---
+++ 18-True-false.js
@@ -0,0 +1,10 @@
+const acesso = 'Salvemos o nosso pais.123456789';
+
+if (acesso == 'Salvemos ao nosso pais.123456789') {
+    console.log('Como você acedrtou?');
+} else {
+    console.log('Você está fora do pário!');
+}
+
+
+
