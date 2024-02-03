ebd616f32b3aa6deaf5c2ece2a29e617529d97a1
Hércules  Ferreira <123484918+haf47@users.noreply.github.com>
2024-02-03 15:01:43 -0300

    Add files via upload

---
+++ 13-exercicios.js
@@ -0,0 +1,19 @@
+console.log('Condições para embarcar no terminal rodoviário de Salvador.');
+const nome = 'Gildenice Pinheiro';
+console.log (nome);
+const exigencia = ('Ser Hercules Ferreira,músico e morar em Salvador.');
+console.log (exigencia);
+
+const nomeDoComprador = ('Nome:Hercules');
+console.log (nomeDoComprador);
+
+console.log ('Destino: Santo Amaro da Purificação.');
+
+if (nome === 'Hercules Ferreira') {
+
+    console.log ('Seja bem vindo e boa viajem');
+    
+} else {
+    console.log ('Venda bloqueada');
+    
+}
